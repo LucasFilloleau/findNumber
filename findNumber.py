bc28ddb68c9a2c2436c4f3ecf0811e1f668406d2
LucasFilloleau <lucas.filloleau@icloud.com>
2025-01-05 15:38:54 +0100

    version fonctionnelle

--- findNumber.py
+++ findNumber.py
@@ -33,48 +33,49 @@ def plusgrand(nbutil, nbordi):
 def verif(nbutil):
     if nbutil >= 1 and nbutil <= 100:
         return True
-    
+
+def partie() :
+    """Jeu "plus grand plus petit", brut"""
+    pseudo = str(input("Entrez vote pseudo :\n"))
+    nbordi = tirerunnombre()
+    nbutil = demandeutilisateur()
+    print(nbordi)
+    essais=1
+
+    while(nbutil != nbordi):
+        essais = essais + 1
+        if plusgrand(nbutil, nbordi) == True:
+            print("Le nombre est plus petit\n")
+            nbutil = demandeutilisateur()
+        else:
+            print("Le chiifre est plus grand\n")
+            nbutil = demandeutilisateur()
+    print("Tu as gagné", pseudo, "! Il t'as fallu", essais, "essais.")
+    # Tableau des scores
+
+    scores.append((pseudo,essais))
+    scores.sort(key=lambda joueur: joueur[1])  # Tri par nombre d'essais
+    print("\nTableau des scores :")
+    for i, (nom, score) in enumerate(scores, start=1):
+        print(i, nom, " - ", score,"essais")
+
+    # boucle du jeu
+    reponse=str(input('''Voulez vous rejouer ? Si oui tapez "Oui" sinon, tapez "Non"\n'''))
+    if reponse == "Oui" or reponse == "oui":
+        partie()
+    else:
+        None
+
 # Définition de la fonction jeu
 
 def jeu():
     """Jeu "plus grand plus petit", complet"""
-    partie = 1
+    numPartie = 1
     try :
-        def jeu1() :
-            """Jeu "plus grand plus petit", brut"""
-            pseudo = str(input("Enterz vote pseudo :\n"))
-            nbordi = tirerunnombre()
-            nbutil = demandeutilisateur()
-            print(nbordi)
-            essais=1
-        
-            while(nbutil != nbordi):
-                essais = essais + 1
-                if plusgrand(nbutil, nbordi) == True:
-                        print("Le chiffre est plus petit\n")
-                else:
-                    print("Le chiifre est plus grand\n")
-                    nbutil = demandeutilisateur()
-                print("Tu as gagné", pseudo, "! Il t'as fallu", essais, "essais.")
-    
-            # Tableau des scores
-        
-            scores.append((pseudo,essais))
-            scores.sort(key=lambda joueur: joueur[1])  # Tri par nombre d'essais
-            print("\nTableau des scores :")
-            for i, (nom, score) in enumerate(scores, start=1):
-                print(i, nom, " - ", score,"essais")
+        partie()
+        numPartie = numPartie + 1
     except ValueError:
-         print("Entrez une réponse valide")
-   
-    # boucle du jeu
-    
-    reponse=str(input('''Voulez vous rejouer ? Si oui tapez "Oui" sinon, tapez "Non"\n'''))
-    if reponse == "Oui" or reponse == "oui":
-        jeu()
-    else:
-        partie = partie + 1
-        None
+        print("Entrez une réponse valide")
 
 # Exécution du jeu
 
